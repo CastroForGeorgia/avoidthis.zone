83ad9a8d6b28669d0c9799c2207caf0b12d07729
andres-stellarly <andres@andrescastro.us>
2025-01-24 23:36:24 -0500

    cleanup env vars for easier swap

--- avoid-this-zone-app/rspack.dev.js
+++ avoid-this-zone-app/rspack.dev.js
@@ -1,12 +1,13 @@
 const path = require('path');
-
 const ReactRefreshPlugin = require('@rspack/plugin-react-refresh');
-
-const {
-  merge
-} = require('webpack-merge');
+const { merge } = require('webpack-merge');
+const { DefinePlugin } = require('@rspack/core'); // Rspack's built-in DefinePlugin
+const dotenv = require('dotenv');
 
 const common = require('./rspack.common.js');
+
+// Load variables from .env into process.env
+dotenv.config();
 
 module.exports = merge(common, {
   mode: 'development',
@@ -14,9 +15,19 @@ module.exports = merge(common, {
   devServer: {
     server: 'https',
     hot: true,
-    static: path.join(__dirname, 'resources', 'public')
+    static: path.join(__dirname, 'resources', 'public'),
   },
   plugins: [
-    new ReactRefreshPlugin()
-  ]
+    new ReactRefreshPlugin(),
+    // Add DefinePlugin to inject environment variables
+    new DefinePlugin({
+      'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+      'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+      'process.env.REACT_APP_FIREBASE_PROJECT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+      'process.env.REACT_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+      'process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.REACT_APP_FIREBASE_APP_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
+      'process.env.REACT_APP_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID),
+    }),
+  ],
 });
--- avoid-this-zone-app/rspack.prod.js
+++ avoid-this-zone-app/rspack.prod.js
@@ -1,12 +1,12 @@
 const path = require('path');
-
 const rspack = require('@rspack/core');
-
-const {
-  merge
-} = require('webpack-merge');
+const { merge } = require('webpack-merge');
+const dotenv = require('dotenv');
 
 const common = require('./rspack.common.js');
+
+// Load variables from .env into process.env
+dotenv.config();
 
 module.exports = merge(common, {
   mode: 'production',
@@ -16,17 +16,27 @@ module.exports = merge(common, {
       patterns: [
         {
           from: path.join(__dirname, 'resources', 'public'),
-          to: '.'
-        }
-      ]
-    })
+          to: '.',
+        },
+      ],
+    }),
+    // Add DefinePlugin for environment variables
+    new rspack.DefinePlugin({
+      'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+      'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+      'process.env.REACT_APP_FIREBASE_PROJECT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+      'process.env.REACT_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+      'process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.REACT_APP_FIREBASE_APP_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
+      'process.env.REACT_APP_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID),
+    }),
   ].filter(Boolean),
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
   },
   optimization: {
     runtimeChunk: {
-      name: 'manifest'
-    }
-  }
+      name: 'manifest',
+    },
+  },
 });
