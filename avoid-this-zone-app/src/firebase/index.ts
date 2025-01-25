e4426f21129b5c88c017f900ce1829105b9c59e3
andres-stellarly <andres@andrescastro.us>
2025-01-25 08:56:46 -0500

    Update index.ts

--- avoid-this-zone-app/src/firebase/index.ts
+++ avoid-this-zone-app/src/firebase/index.ts
@@ -2,4 +2,7 @@ export { app } from "./app";
 export { analytics } from "./analytics";
 export { auth, signInAnonymouslyIfNeeded } from "./auth";
 export { db, fetchEnumValues } from "./firestore";
-export { functions, createRaidReport, CreateRaidReportPayload } from "./functions";
+export { functions, createRaidReport } from "./functions";
+
+// Separate type-only exports
+export type { CreateRaidReportPayload } from "./functions";
